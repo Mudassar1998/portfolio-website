23682a0169bf013d21a50aa75476b7912b485dae
mudassar saleem <khanqureshi31181@gmail.com>
2023-03-03 18:15:41 -0600

    added files and working on nav

---
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.scss'
+import { images } from '../../constants'
+
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div>
+          <img src={images.logo} alt='logo' />
+        </div>
+        <ul>
+          {['home', 'about', ,'work', 'skills', 'contact'].map((item) => (
+            <li key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}> {item}</a>
+            </li>
+          ))}
+      </ul>
+      </nav>
+
+    </>
+  )
+}
+
+export default Navbar
